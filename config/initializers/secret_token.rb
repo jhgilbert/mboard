f7f7306d9c7a6bd111c99f343b4e3679bb696b86
jhgilbert <j.h.gilbert@gmail.com>
2012-11-06 17:03:12 -0800

    Save before adding posts functionality

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mboard::Application.config.secret_token = '786e80484fa46dcad2b20cf0d33d84c743c109f7fc69a04af0e98d09c65d4005fdbca2dae2977cd0a60177109e6efb03b9ec39a3d7df12ddbb33dac6ffe55e48'
